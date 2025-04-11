da2e60f8e56d227e79ba4f270d5cd184a1281d94
cityhand <vuilleminethan@gmail.com>
2025-04-11 12:13:50 +0200

    Avancement projet 6 - Fin  des scripts

---
+++ Projet 6 - Classifiez automatiquement des biens de consommation/notebooks/Vuillemin_Ethan_3_script_Python_11042025.py
@@ -0,0 +1,43 @@
+#%%
+import requests
+import csv
+
+# Setup les parms de la query
+url = "https://world.openfoodfacts.org/cgi/search.pl"
+params = {
+    "search_terms": "champagne",
+    "page_size": 10,
+    "json": 1
+}
+# faire la requete
+response = requests.get(url, params=params)
+data = response.json()
+#%%
+# Recuperer les champs qui nous interessent
+products = []
+for product in data.get("products", []):
+    food_id = product.get("_id", "")
+    label = product.get("product_name", "")
+    category = product.get("categories", "")
+    food_contents_label = product.get("ingredients_text", "")
+    image = product.get("image_front_url", "")
+    
+    products.append({
+        "foodId": food_id,
+        "label": label,
+        "category": category,
+        "foodContentsLabel": food_contents_label,
+        "image": image
+    })
+
+# Creation du fichier CSV
+csv_filename = f"produits_{params['search_terms']}.csv"
+csv_headers = ["foodId", "label", "category", "foodContentsLabel", "image"]
+
+with open(csv_filename, mode="w", newline="", encoding="utf-8") as file:
+    writer = csv.DictWriter(file, fieldnames=csv_headers)
+    writer.writeheader()
+    writer.writerows(products)
+
+print(f"Les données ont été exportées dans le fichier '{csv_filename}'.")
+# %%
